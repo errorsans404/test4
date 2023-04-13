0bffb502ebd0a4e4ae70b34a49b9391574a4fe37
LerOni <v.e.shulgina@gmail.com>
2023-04-13 16:35:11 +0700

    v1

---
+++ test3/test3/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length < 2 || textBox1.Text.Length > 25)
+            {
+                button1.Text = "город";
+                return;
+            }
+
+            if (textBox2.Text.Length < 10 || textBox2.Text.Length > 50)
+            {
+                button1.Text = "улица";
+                return;
+            }
+
+            if (textBox3.Text.Length != 6)
+            {
+                button1.Text = "почт индекс";
+                return;
+            }
+
+            try
+            {
+                int x = Int32.Parse(textBox3.Text);
+            }
+
+            catch(Exception ex)
+            {
+                button1.Text = "-почт индекс";
+                return;
+            }
+
+        }
+    }
+}
